ac81e67cba3c3bb16825503e3e47e253e0f75b2c
syom29 <syom29@gmail.com>
2019-06-27 20:58:19 +0300

    First commit

---
+++ src/login_dark_theme/FXMLDocumentController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_dark_theme;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class FXMLDocumentController implements Initializable
+{   
+    @FXML
+    private void handleClose()
+    {
+        System.exit(0);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        
+    }    
+    
+}
